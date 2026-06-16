6e64fd815cd2869849478d9147abf3be4d4f8b7a
Dhananjay Diwan <djdiwan@yahoo.com>
2026-06-16 11:06:24 +0000

    Added Product Listing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,19 @@
 package com.mycompany.app;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product("Mi11X", 28000));
+        products.add(new Product("Redme Note10 Pro", 22000));
+        products.add(new Product("Motorola G30", 30000));
+        System.out.println("Product List:");
+        for (Product product : products) {
+            System.out.println(product.getName() + " - " + product.getPrice());
+        }
     }
 }

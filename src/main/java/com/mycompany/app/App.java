75638f48eeed891fe7ddd7d43a4d41bf09132bf9
Dhananjay Diwan <djdiwan@yahoo.com>
2026-06-16 11:12:12 +0000

    Updated code docs

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,9 +3,15 @@ package com.mycompany.app;
 import java.util.ArrayList;
 
 /**
- * Hello world!
+ * The App class serves as the entry point for the application.
+ * It demonstrates the creation and display of a list of products.
  */
 public class App {
+    /**
+     * The main method initializes a list of products and prints their details to the console.
+     *
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
         ArrayList<Product> products = new ArrayList<>();
         products.add(new Product("Mi11X", 28000));
